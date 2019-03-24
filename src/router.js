67d773c68ff7aa48fd7a432ef2257ed887742c05
luyu <2576564946@qq.com>
2019-03-24 16:54:59 +0800

    新增表格、输入框

--- src/router.js
+++ src/router.js
@@ -7,6 +7,8 @@ import Login from "./view/login";
 import NoPage from "./view/noPage";
 import Bottons from "./view/bottons";
 import Forms from "./view/forms";
+import Tables from './view/tables'
+import Inputs from './view/inputs'
 import Home from './view/home'
 export default class Router extends Component{
     render(){
@@ -20,6 +22,8 @@ export default class Router extends Component{
                               <Route path='/admin/home' component={Home}></Route>
                               <Route path='/admin/ui/button' component={Bottons}></Route>
                               <Route path='/admin/ui/form' component={Forms}></Route>
+                              <Route path='/admin/ui/inputs' component={Inputs}></Route>
+                              <Route path='/admin/ui/tables' component={Tables}></Route>
                               <Route component={NoPage}></Route>
                           </Switch>
                        </Admin>
